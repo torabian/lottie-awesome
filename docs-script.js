dd0576e4571d4348983d41bf848ca2fd2d863558
Ali <a@a.com>
2019-06-08 17:01:43 +0200

    Add the fonts

---
+++ docs-script.js
@@ -0,0 +1,55 @@
+const getButtonUrl = button => {
+  return `https://raw.githubusercontent.com/torabian/rovx-gamepad/master/src/components/GamepadButton/json/btn-${button}.json`;
+};
+function randomIntFromInterval(min, max) {
+  // min and max included
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+function renderButtons(list) {
+  for (let item of list) {
+    fetch(getButtonUrl(item)).then(async data => {
+      const animData = await data.json();
+      setTimeout(() => {
+        render(item, animData);
+      }, randomIntFromInterval(200, 900));
+    });
+  }
+}
+renderButtons([
+  'circle',
+  'rectangle',
+  'cross',
+  'triangle',
+  'up',
+  'down',
+  'left',
+  'right'
+]);
+
+function render(buttonName, data) {
+  let animation = bodymovin.loadAnimation({
+    container: document.getElementById(buttonName),
+    renderer: 'svg',
+    autoplay: true,
+    loop: false,
+    animationData: data
+  });
+  // Wait for all animation data to be loaded in DOM
+  animation.addEventListener('DOMLoaded', function() {
+    let isRevert = true;
+    // Once animation is complete go back to frame 0 on timeline
+    animation.addEventListener('complete', function() {
+      setTimeout(() => {
+        if (isRevert) {
+          animation.setDirection(-1);
+          animation.play();
+          isRevert = false;
+        } else {
+          animation.setDirection(1);
+          animation.goToAndPlay(0);
+          isRevert = true;
+        }
+      }, 3000);
+    });
+  });
+}
